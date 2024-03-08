dacff1d7a41bba6c57005cbc8312e06fe0637b48
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-08 22:06:25 +0100

    Add files via upload

---
+++ Chapter_02/Chap_02_Ex_01/main.cpp
@@ -0,0 +1,15 @@
+/*
+Chapter 02 Exercise 01
+
+Napište program v jazyce C++, který vypíše vaše jméno a adresu.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Name Vorname\n";
+	cout << "Address";
+	return 0;
+}
