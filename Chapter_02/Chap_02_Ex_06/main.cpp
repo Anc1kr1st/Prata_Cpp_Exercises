001719a57faef1e9efaea600c85182e0ea475510
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-08 23:04:10 +0100

    Add files via upload

---
+++ Chapter_02/Chap_02_Ex_06/main.cpp
@@ -0,0 +1,31 @@
+﻿/*
+Chapter 02 Exercise 06
+
+Napište program, který si vyžádá hodnotu hodiny a hodnotu minuty. Funkce main() pak tyto hodnoty předá funkci typu void, 
+která je vypíše v následujícím tvaru:
+Enter the number of hours: 9
+Enter the number of minutes: 28
+Time 9:28
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int hours;
+int minutes;
+
+void time() {
+	cout << "Time: " << hours << ":" << minutes;
+}
+
+int main() {
+	cout << "Enter the number of hours: ";
+	cin >> hours;
+	cout << "Enter the number of minutes: ";
+	cin >> minutes;
+
+	time();
+
+	return 0;
+}
