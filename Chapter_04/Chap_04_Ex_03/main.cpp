9b5c6e17918f69100c7d3d6411fb63e1aab88651
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-19 18:50:30 +0100

    Add files via upload

---
+++ Chapter_04/Chap_04_Ex_03/main.cpp
@@ -0,0 +1,32 @@
+﻿/*
+Ch4 Ex3
+3.  Napište program, který si vyžádá od uživatele křestní jméno a  příjmení a  poté vytvoří, 
+uloží a vypíše třetí řetězec, který se bude skládat z příjmení, čárky, mezery a křestního jména.
+Použijte k tomu pole char a funkce z hlavičkového souboru cstring.Výpis po spuštění by mohl vypadat 
+například takto :
+Zadejte krestni jmeno : Flip
+Zadejte prijmeni : Fleming
+Zde jsou oba udaje spojeny do jednoho retezce : Fleming, Flip
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+	char firstName[20];
+	char lastName[20];
+
+	cout << "Enter first name: ";
+	cin >> firstName;
+	cout << "Enter last name: ";
+	cin >> lastName;
+
+	strcat_s(lastName, ", ");
+	strcat_s(lastName, firstName);
+	cout << "Here, both data are combined into one string: " << lastName;
+
+	return 0;
+}
