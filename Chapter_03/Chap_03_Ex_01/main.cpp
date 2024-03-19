428347fe63f8aa20c2a057696cd6d1f5486de3a8
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-19 14:07:06 +0100

    Add files via upload

---
+++ Chapter_03/Chap_03_Ex_01/main.cpp
@@ -0,0 +1,23 @@
+﻿/*
+Chapter 03 Exercise 01
+
+Napište krátký program, který se zeptá na vaši výšku v celých palcích, a potom ji konvertuje 
+na stopy a palce. Podtržítky vyznačte místa pro vepsání zadání. Pro konverzní faktor použijte 
+symbolickou konstantu uvozenou klíčovým slovem const.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int inches;
+	const int conversion = 12;
+
+	cout << "What is your height in inches?\n";
+	cin >> inches;
+	cout << "Your height is " << inches / conversion << " feet and " << inches % conversion << " inch(es).";
+
+	return 0;
+}
