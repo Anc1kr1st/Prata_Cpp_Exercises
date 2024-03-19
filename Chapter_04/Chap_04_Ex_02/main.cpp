cfa3ee1ffe4814de67e442d48e25fb1bf4bc32df
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-19 18:16:03 +0100

    Add files via upload

---
+++ Chapter_04/Chap_04_Ex_02/main.cpp
@@ -0,0 +1,24 @@
+﻿/*
+Ch4 Ex2
+2.  Přepište výpis 4.4., místo polí char použijte třídy string jazyka C++.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string name;
+	string dessert;
+
+	cout << "Zadejte vase jmeno:\n";
+	getline(cin, name);
+	cout << "Zadejte vas oblubeny zakusek:\n";
+	getline(cin, dessert);
+	cout << "Mam vyborny " << dessert;
+	cout << ", ktery si zaslouzi jen " << name << ".";
+
+	return 0;
+}
