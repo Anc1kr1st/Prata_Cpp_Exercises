909c1c335b32c1d178c6491eb379d1d1d6f18ebc
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-19 16:55:18 +0100

    Add files via upload

---
+++ Chapter_03/Chap_03_Ex_04/main.cpp
@@ -0,0 +1,30 @@
+﻿/* 4.  Napište program, který si vyžádá vstup počtu sekund jako celočíselnou hodnotu (použijte typ long) a pak 
+vypíše ekvivalent ve dnech, hodinách, minutách a sekundách.Počty hodin ve dni, minut v hodině a sekund v minutě 
+nech  jsou dány symbolickými konstantami. Po spuštění by se mělo vypsat :
+Zadejte počet sekund : 31600000
+31600000 sekund = 365 dní, 46 minut, 40 sekund
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	long input, days, hours, minutes, seconds;
+	int const cHours = 24;
+	int const cMinutes = 60;
+	int const cSeconds = 60;
+
+	cout << "Enter number of seconds: ";
+	cin >> input;
+	days = ((input / cSeconds) / cMinutes) / cHours;
+	hours = ((input / cSeconds) / cMinutes) % cHours;
+	minutes = (input / cSeconds) % cMinutes;
+	seconds = input % cSeconds;
+
+	cout << input << " seconds = " << days << " days, " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds";
+
+	return 0;
+}
+
