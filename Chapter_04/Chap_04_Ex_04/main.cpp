a410f1525d19c3f57f77150523dea4ea047f4418
Richard <134712909+Anc1kr1st@users.noreply.github.com>
2024-03-19 18:58:24 +0100

    Add files via upload

---
+++ Chapter_04/Chap_04_Ex_04/main.cpp
@@ -0,0 +1,32 @@
+﻿/*
+Ch4EX4
+
+4.  Napište program, který si vyžádá od uživatele křestní jméno a  příjmení a  poté vytvoří, 
+uloží a vypíše třetí řetězec, který se bude skládat z příjmení, čárky, mezery a křestního jména.
+Použijte k tomu objekty string a metody z hlavičkového souboru string. Výpis po spuštění by mohl 
+vypadat například takto :
+Zadejte krestni jmeno : Flip
+Zadejte prijmeni : Fleming
+Zde jsou oba udaje spojeny do jednoho retezce : Fleming, Flip
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string firstName;
+	string lastName;
+
+	cout << "Enter first name: ";
+	cin >> firstName;
+	cout << "Enter last name: ";
+	cin >> lastName;
+
+	string fullName = lastName + ", " + firstName;
+	cout << fullName;
+
+	return 0;
+}
